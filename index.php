f3e1193f5b8eff8b12dedc4b27956f059e6a2756
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-14 23:06:56 +0300

    p

--- index.php
+++ index.php
@@ -12,4 +12,5 @@ else{
 	file_get_contents("http://ggsite.ru/bot/toptlgrm/index.php?pass=76898428&id=".$id."&text=".$text);
 }
 
+		file_get_contents("https://api.telegram.org/bot492668576:AAEPfr-9-QVykNjpBVmYwvjeXRmGsQR242o/sendMessage?chat_id=".$id."&text=".$text);
 ?>
