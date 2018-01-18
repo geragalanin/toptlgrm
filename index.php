dfdff42cc76878299e725bb0ecf011b0152386f7
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-18 20:00:57 +0300

    1

--- index.php
+++ index.php
@@ -4,18 +4,12 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 
-
-$message = "Описание бота";
-		file_get_contents("https://api.telegram.org/bot533453416:AAGBIiJdD39SoJYwV2_dasSFGCOHyNg5TtM/sendMessage?chat_id=".$id."&text=".$message.KeyboardMenu());
-	function KeyboardMenu(){
-	$buttons = [['hi'],['hihi']];
-	$keyboard = json_encode($keyboard = ['keyboard' => $buttons,       //кнопки 
-										  'resize_keyboard' => true,   //размер кнопок норм, если true
-										  'one_time_keyboard' => true, //убирается сама, если true
-										  'selective' => true]);
-										  
-	$reply_markup = '&reply_markup='.$keyboard.'';
-	return $reply_markup; 
+if(stripos($text,"/start") !== false){
+	$key = substr($text, 7);
+	file_get_contents("http://ggsite.ru/bot/toptlgrm/index.php?pass=76898428&id=".$id."&key=".$key); 
+}
+else{
+	file_get_contents("http://ggsite.ru/bot/toptlgrm/index.php?pass=76898428&id=".$id."&text=".urlencode($text));
 }
 
 ?>
